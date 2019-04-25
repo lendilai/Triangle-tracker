3308439109351f41cf31534434914e0dcf5d2422
lendilai <ngethenan768@gmail.com>
2019-04-25 14:49:47 +0300

    Adds HTML info and first variables of JS

---
+++ script.js
@@ -0,0 +1,5 @@
+function tracker(){
+  var sideA = parseFloat(document.getElementById('a').value;
+  var sideB = parseFloat(document.getElementById('b').value;
+  var sideC = parseFloat(document.getElementById('c').value;
+}
