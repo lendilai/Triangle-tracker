80f6eb4d625d784465db38b9b2260109e1f04fbb
lendilai <ngethenan768@gmail.com>
2019-04-25 14:55:11 +0300

    Added the first 'if' statement

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ function tracker(){
   var sideA = parseFloat(document.getElementById('a').value;
   var sideB = parseFloat(document.getElementById('b').value;
   var sideC = parseFloat(document.getElementById('c').value;
+  var text;
+}
+if ((a + b <= c) || (a + c <= b) || (b + c <= a)){
+    text = document.getElementById('result').innerHTML = "This is not a triangle!";
 }
