38107d7dffbfcfa164ef02b6005cfb9cbfa4cd20
lendilai <ngethenan768@gmail.com>
2019-04-25 19:20:37 +0300

    Added stetements for scalene and right-angled triangles

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function tracker(){
-  var sideA = parseFloat(document.getElementById('a').value;
-  var sideB = parseFloat(document.getElementById('b').value;
-  var sideC = parseFloat(document.getElementById('c').value;
+  var sideA = parseFloat(document.getElementById("a").value);
+  var sideB = parseFloat(document.getElementById("b").value);
+  var sideC = parseFloat(document.getElementById("c").value);
   var text;
-}
-
-
-if ((sideA + sideB <= sideC) || (sideA + sideC <= sideB) || (sideB + sideC <= sideA)){
+  if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA){
     text = document.getElementById('result').innerHTML = "This is not a triangle!";
 }
-  else if ((sideA === sideB && sideA === sideC && sideB === sideC)){
-    text = document.getElementById('result').innerHTML = "This is an equilateral triangle";
-  }
-  else if (sideA === sideB || sideA === sideC || sideB === sideC){
-    text = document.getElementById('result').innerHTML = "This is an isosceles triangle";
+      else if (sideA === sideB && sideA === sideC && sideB === sideC){
+        text = document.getElementById('result').innerHTML = "This is an equilateral triangle";
+      }
+      else if (sideA === sideB || sideA === sideC || sideB === sideC){
+        text = document.getElementById('result').innerHTML = "This is an isosceles triangle";
+      }
+      else if (sideA**2 + sideB**2 === sideC**2){
+        text = document.getElementById('result').innerHTML = "This is a right-angled triangle.";
+      }
+      else {
+        text = document.getElementById('result').innerHTML = "This is a scalene triangle.";
+      }
   }
