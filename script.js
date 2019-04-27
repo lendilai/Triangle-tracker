db711970c246151653712e8e0f67b6769a4e7f84
lendilai <ngethenan768@gmail.com>
2019-04-27 15:54:36 +0300

    Added triangle images when submission is done

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ function tracker(){
   var text;
   var sides = ["sideA", "sideB", "sideC", "text"]
   if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA){
-    text = document.getElementById('result').innerHTML = '<strong>This is not a triangle!</strong>';
+    text = document.getElementById('result').innerHTML = '<strong>This is not a triangle!</strong> <br> <img src="Images/not.jpg">';
 }
       else if (sideA === sideB && sideA === sideC && sideB === sideC){
-        text = document.getElementById('result').innerHTML = 'This is an <strong>equilateral</strong> triangle';
+        text = document.getElementById('result').innerHTML = 'This is an <strong>equilateral</strong> triangle <br> <img src="Images/equilateral.png">';
       }
       else if (sideA === sideB || sideA === sideC || sideB === sideC){
-        text = document.getElementById('result').innerHTML = 'This is an <strong>isosceles</strong> triangle';
+        text = document.getElementById('result').innerHTML = 'This is an <strong>isosceles</strong> triangle <br> <img src="Images/isosceles.png">';
       }
       else if (sideA**2 + sideB**2 === sideC**2){
-        text = document.getElementById('result').innerHTML = 'This is a <strong>right-angled</strong> triangle.';
+        text = document.getElementById('result').innerHTML = 'This is a <strong>right-angled</strong> triangle.  <br> <img src="Images/right.png">';
       }
         else {
-          text = document.getElementById('result').innerHTML = 'This is a <strong>scalene</strong> triangle.';
+          text = document.getElementById('result').innerHTML = 'This is a <strong>scalene</strong> triangle. <br> <img src="Images/scalene.jpg">';
         }
   }
   var triangle = function(){
